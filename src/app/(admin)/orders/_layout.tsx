fdbde69762238e9aceb5a8db952abfc54489ed36
Sulvid <45789416+SulvidPvtLtd@users.noreply.github.com>
2024-10-13 11:57:02 +0200

    Custom Nav -Active & Archive - completed

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from 'expo-router';
 export default function MenuStack() {
   return (
     <Stack >
-        <Stack.Screen name="index" options={{title:'Order Items', headerTitleStyle:{color:Colors.light.adminBtn, fontSize: 20}}}/>
+        {/*<Stack.Screen name="index" options={{title:'Order Items', headerTitleStyle:{color:Colors.light.adminBtn, fontSize: 20}}}/>*/}
         <Stack.Screen name="list" options={{headerShown:false}}/>
     </Stack>
   )
--- src/app/(admin)/orders/list/_layout.tsx
+++ src/app/(admin)/orders/list/_layout.tsx
@@ -10,12 +10,16 @@
 import { withLayoutContext } from "expo-router";
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { SafeAreaView } from "react-native-safe-area-context";
+
 const TopTabs = withLayoutContext(createMaterialTopTabNavigator().Navigator);
 
 export default function OrderListNavigator(){
     return(
-        <SafeAreaView>
-            <TopTabs/>
+        <SafeAreaView edges={["top"]} style={{flex:1, backgroundColor:"white"}}>
+            <TopTabs>
+                <TopTabs.Screen name="index" options={{title:"Active"}}/>
+                {/*<TopTabs.Screen name="archive" options={{title:"Archive"}}/>"*/}
+            </TopTabs>
         </SafeAreaView>
     )
 }
